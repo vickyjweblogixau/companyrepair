c4dc27b8f66c6c9de38b8c0833b62448c9aec9ff
Vicky <vigneshwari@weblogix.com.au>
2026-06-22 12:11:53 +0000

    june-22:5:45

--- staging01/wp-content/plugins/crs-business-dashboard/inc/class-crs-rewrite.php
+++ staging01/wp-content/plugins/crs-business-dashboard/inc/class-crs-rewrite.php
@@ -11,32 +11,49 @@ class CRS_Rewrite {
 
     public static function add_rewrite_rules() {
         add_rewrite_rule(
-            '^business/([^/]+)/?$',
-            'index.php?crs_enquire=1&crs_business_slug=$matches[1]',
+            '^enquir[ey]/([^/]+)/?$',
+            'index.php?crs_enquiry_slug=$matches[1]',
             'top'
         );
     }
 
     public static function add_query_vars( $vars ) {
-        $vars[] = 'crs_enquire';
-        $vars[] = 'crs_business_slug';
+        $vars[] = 'crs_enquiry_slug';
         return $vars;
     }
 
     public static function load_enquiry_template() {
-        if ( get_query_var( 'crs_enquire' ) ) {
-            $template = CRS_PLUGIN_DIR . 'templates/enquiry-page.php';
-            if ( file_exists( $template ) ) {
-                include $template;
-                exit;
-            }
+        $slug = get_query_var( 'crs_enquiry_slug' );
+        if ( ! $slug ) return;
+
+        $business = get_page_by_path( $slug, OBJECT, 'business' );
+
+        if ( ! $business ) {
+            global $wp_query;
+            $wp_query->set_404();
+            status_header( 404 );
+            get_template_part( '404' );
+            exit;
+        }
+
+        $template = CRS_PLUGIN_DIR . 'templates/enquiry-page.php';
+
+        if ( file_exists( $template ) ) {
+            global $post;
+            $post = $business;
+            setup_postdata( $post );
+            set_query_var( 'crs_enquiry_slug_business', $business );
+            include $template;
+            exit;
         }
     }
 }
 
 CRS_Rewrite::init();
+
+// Prevent WordPress canonical redirect from hijacking our URL
 add_filter( 'redirect_canonical', function( $redirect_url, $requested_url ) {
-    if ( get_query_var( 'crs_enquire' ) ) {
+    if ( get_query_var( 'crs_enquiry_slug' ) ) {
         return false;
     }
     return $redirect_url;
